04bbcf4f29e07cc417dc8463e3208ace2fc39a16
Mukul <mukuldixit2782@gmail.com>
2020-07-09 15:10:57 +0530

    Added feature of multiple pages

--- proj.py
+++ proj.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-page = Image.open('page.png').convert('L')
+page = Image.open('page.png')
 #text=input("Enter text: ")
 
 path_font = 'myfont/'#
@@ -13,31 +13,49 @@ text = fp.read()
 
 line_num=0
 let_num=0
+pagenum=0
 para_list=text.split('\n')
 for para in para_list:
+
     word_list = para.split(' ')
+    
     for word in word_list:
         wsize = len(word)
+        
         if(wsize*width >= 2480 - let_num*width):
             line_num+=1
             let_num=0
+            
+        if((line_num+1)*length>= 3508):
+            page.save('result'+str(pagenum)+'.png')
+            page=Image.open('page.png')
+            pagenum+=1
+            line_num=0
+            let_num=0
+        
         for i in range(wsize):
             letter = word[i]
             name = letter
+            
+            print(name,end='')
+            
             if(name.isupper()):
                 name=name.lower()+'upper'
             elif(name=='?'):
                 name='question'
-            print(name,end='')
-            let_image = Image.open(path_font + name + '.png').convert('L')
+            let_image = Image.open(path_font + name + '.png')
             rect=(let_num*width,line_num*length)
             page.paste(let_image,rect)
             let_num+=1
+        
         rect=(let_num*width,line_num*length)
-        let_image = Image.open(path_font+'space.png').convert('L')
+        let_image = Image.open(path_font+'space.png')
         page.paste(let_image,rect)
         let_num+=1
         print(" ",end='')
-page.save('result.png')
+    line_num+=1        
+    let_num=0
+    print('\n',end='')
+page.save('result'+str(pagenum)+'.png')
             
         
